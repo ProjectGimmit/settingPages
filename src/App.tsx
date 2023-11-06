4732e49b7c2d3084397420ab66e1002403f567cc
kaizuchan <kaizu715@icloud.com>
2023-11-06 10:48:18 +0900

    ルーティングの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Top from './routes/Top';
+import Config from './routes/Config';
+import Manual from './routes/Manual';
+import Nomatch from './routes/Nomatch';
+// import About from './routes/about';
+// import Contact from './routes/contact';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Top />} />
+        <Route path="/config" element={<Config />} />
+        <Route path="/manual" element={<Manual />} />
+        <Route path="*" element={<Nomatch />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
