a767165b1e641f2a575f09afd7b363af0a4116e3
kaizuchan <kaizu715@icloud.com>
2023-11-26 15:10:19 +0900

    マニュアルページの枠組み(説明部分未実装)

--- src/routes/Manual.tsx
+++ src/routes/Manual.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Accordion, AccordionHeader } from 'react-bootstrap';
+import { fetchDayAlarmSettingsFromAPI } from '../api/alarmApi';
 
-const manual = () => {
+const Manual = () => {
+  
+  const [gimmickData, setGimmickData] = useState<{ [key: string]: { enable: boolean; answer?: boolean[] | number; pattern?: boolean[]; default?: boolean[]; }; } | null>(null);
+
+  useEffect(() => {
+    // 曜日ごとの設定情報をAPIから取得
+    fetchDayAlarmSettingsFromAPI({day:'mon'})
+      .then(data => setGimmickData(data.gimmick))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div>manual</div>
-  )
-}
+    <div>
+      <h1>Gimmit</h1>
+      <p>今日のマニュアル</p>
+      <Accordion alwaysOpen>
+        {gimmickData && Object.keys(gimmickData).map((key) => 
+          gimmickData[key].enable && <Accordion.Item eventKey={key} key={key}>
+            <AccordionHeader>
+              {key === 'wires' && <p>ワイヤー</p>}
+              {key === 'toggleSW' && <p>トグルスイッチ</p>}
+              {key === 'keySW' && <p>キースイッチ</p>}
+              {key === 'lightsOut' && <p>ライツアウト</p>}
+              {key === 'level' && <p>レベルメーター</p>}
+            </AccordionHeader>
+            <Accordion.Body>
+              <p>{key}の説明</p>
+            </Accordion.Body>
+          </Accordion.Item>
+        )}
+      </Accordion>
+    </div>
+  );
+};
 
-export default manual
+export default Manual;
